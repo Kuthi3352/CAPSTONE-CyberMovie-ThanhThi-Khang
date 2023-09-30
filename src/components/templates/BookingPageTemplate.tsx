9db245e2be92d4c6c79bb194d397225e258fb830
ThanhThi <anhthidola@gmail.com>
2023-09-30 12:44:15 +0700

    1

--- src/components/templates/BookingPageTemplate.tsx
+++ src/components/templates/BookingPageTemplate.tsx
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { RootState, useAppDispatch } from "store";
-import { ThongTinPhimThunk } from "store/Booking/thunk";
+import { DanhSachGheThunk } from "store/Booking/thunk";
 // import { DanhSachGheThunk } from "store/Booking/thunk";
 // import { ThongTinLichChieuThunk } from "store/QuanLyRap/thunk";
 
@@ -13,18 +13,18 @@ import { ThongTinPhimThunk } from "store/Booking/thunk";
 export const BookingPageTemplate = () => {
   //tao moi truong 
   const dispatch = useAppDispatch()
-  const { thongTinPhimRap } = useSelector(
+  const { danhSachGhe } = useSelector(
     (state: RootState) => state.DanhSachPhongVe
   );
   const { lichchieuID } = useParams()
 
   //theo doi 
-  console.log('thongTinPhimRap', thongTinPhimRap);
+  console.log('thongTinPhimRap', danhSachGhe);
 
 
   //goi API 
   useEffect(() => {
-    dispatch(ThongTinPhimThunk(lichchieuID))
+    dispatch(DanhSachGheThunk(lichchieuID))
   }, [dispatch, lichchieuID])
 
 
