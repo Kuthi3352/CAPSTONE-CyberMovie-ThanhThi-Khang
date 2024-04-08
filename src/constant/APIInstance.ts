61365fa8b536c2f6257dbedeb760d03ec91b416a
Kuthi <anhthidola@gmail.com>
2024-04-08 21:54:38 +0700

    owari

--- src/constant/APIInstance.ts
+++ src/constant/APIInstance.ts
@@ -2,7 +2,7 @@ import axios, { CreateAxiosDefaults, AxiosRequestHeaders } from "axios";
 import { getAccessToken } from "utils";
 
 const TokenCybersoft =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCA0NyIsIkhldEhhblN0cmluZyI6IjAxLzAyLzIwMjQiLCJIZXRIYW5UaW1lIjoiMTcwNjc0NTYwMDAwMCIsIm5iZiI6MTY3ODk4NjAwMCwiZXhwIjoxNzA2ODkzMjAwfQ.-axBsmkeW5i-ufzRXjqOhPEUumPXCQLTot5UjBRmtdQ";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJOb2RlSlMgMzgiLCJIZXRIYW5TdHJpbmciOiIxNC8wNy8yMDI0IiwiSGV0SGFuVGltZSI6IjE3MjA5MTUyMDAwMDAiLCJuYmYiOjE3MDI0ODY4MDAsImV4cCI6MTcyMTA2MjgwMH0.cB5XSbdlq0lzL-wmbcuAyvlRLMYFWmr20ODRWN5rPZc";
 
 export const apiInstance = (config?: CreateAxiosDefaults) => {
   const api = axios.create(config);
@@ -11,7 +11,7 @@ export const apiInstance = (config?: CreateAxiosDefaults) => {
       ...config,
       headers: {
         TokenCybersoft,
-        Authorization: 'Bearer '+ getAccessToken()||''
+        Authorization: "Bearer " + getAccessToken() || "",
       } as unknown as AxiosRequestHeaders,
     };
   });
